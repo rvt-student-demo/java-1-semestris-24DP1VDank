c0c3402f8d1488d9f2d05f3b20878dc3737cfdec
Viktorija <A240310vd@rvt.lv>
2025-09-30 13:57:42 +0300

    From parametr to one uzdevums

---
+++ src/main/java/rvt/from_parametr_to_one.java
@@ -0,0 +1,13 @@
+package rvt;
+
+public class from_parametr_to_one {
+    public static void main(String[] args) {
+        printFromNumberToOne(2);
+    }
+
+    public static void printFromNumberToOne(int number){
+        for(int i = number; i >= 1; i--){
+            System.out.println(i);
+    }
+}
+}
