2d5e03b5f6d0d8ce8bfb37b55448ceca02ad9dc8
Viktorija <A240310vd@rvt.lv>
2025-12-10 21:51:21 +0200

    Overloaded_Counter

--- src/main/java/rvt/Overloaded_Counter.java
+++ src/main/java/rvt/Overloaded_Counter.java
@@ -4,5 +4,45 @@ package rvt;
 // Overloaded Counter uzdevums
 
 public class Overloaded_Counter {
-    
+    int startValue;
+
+    public static void main(String[] args) {
+        Overloaded_Counter a = new Overloaded_Counter();
+        a.increase(10);
+        a.increase(-5);
+        a.decrease(2);
+        a.decrease(-10);
+        System.out.println(a);
+    }
+
+
+    public Overloaded_Counter(int startValue){
+        this.startValue = startValue;
+    }
+
+    public Overloaded_Counter(){
+        startValue = 0;
+    }
+
+    public int value(){
+        return startValue;
+    }
+
+    public void increase(int increaseBy){
+        if(increaseBy > 0){
+            startValue += increaseBy;
+        }
+
+        
+    }
+
+    public void decrease(int decreaseBy){
+        if(decreaseBy > 0){
+            startValue -= decreaseBy;
+        }
+
+}
+    public String toString() {  
+        return "Counter value: " + startValue;
+    }
 }
