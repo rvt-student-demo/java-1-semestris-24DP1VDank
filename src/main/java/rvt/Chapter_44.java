bcd38f4013190ef0ebd98d92db32201dead0ef76
Viktorija <A240310vd@rvt.lv>
2025-12-10 10:44:28 +0200

    Chapter_44

--- src/main/java/rvt/Chapter_44.java
+++ src/main/java/rvt/Chapter_44.java
@@ -1,8 +1,40 @@
 package rvt;
+import java.util.Scanner;
+
 
 // chapter 44
 // chapter 44 uzdevums
 
+
 public class Chapter_44 {
-    
-}
+    public static void main(String[] args) {
+    // Echo();
+    CharAt();
+    }
+
+
+    public static void Echo(){
+        System.out.print("Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        String fulname = sc.nextLine();
+
+        //String fulname = "Laalalla Kkfsdlfj";
+        int index_surname = fulname.indexOf(" ");
+        String name = fulname.substring(0, index_surname);
+        String surname = fulname.substring(index_surname).toUpperCase(); 
+    System.out.println(name + surname);    
+    sc.close();
+    }
+
+    public static void CharAt(){
+        System.out.print("Enter a String: ");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+
+        }
+        sc.close();
+    }}
+
